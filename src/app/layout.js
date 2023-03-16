692e14388a2cc82fef1ae6688eb68a5326836d49
RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>
2023-03-17 01:00:30 +0530

    created header section
    Signed-off-by: RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,24 @@
+import Header from '@/components/Header'
 import './globals.css'
 
 export const metadata = {
   title: 'Movies',
-  description: 'See your Favourite Movie',
+  description: 'See your Favorite Movie',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      {/* Header */}
+       <Header/>
+      
+      {/* Navbar */}
+
+      {/* SearchBox */}
+
+      {children}
+      </body>
     </html>
   )
 }
