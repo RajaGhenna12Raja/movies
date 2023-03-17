fa78a76e866f57c038ccf045bcbea09b35aed365
RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>
2023-03-17 11:04:33 +0530

    added Navbar
    Signed-off-by: RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import Header from '@/components/Header'
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import Providers from './providers'
 
@@ -17,7 +18,8 @@ export default function RootLayout({ children }) {
        <Header/>
       
       {/* Navbar */}
-
+        <Navbar/>
+        
       {/* SearchBox */}
 
       {children}  
