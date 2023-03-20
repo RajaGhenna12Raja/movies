d2d61153b5fcdf95b6d3dd472f2fcfb4691e64d0
RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>
2023-03-20 13:49:33 +0530

    fetched Api
    Signed-off-by: RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,28 @@
-import Header from '@/components/Header'
-import Navbar from '@/components/Navbar'
-import './globals.css'
-import Providers from './providers'
-
+import Header from '@/components/Header';
+import Navbar from '@/components/Navbar';
+import './globals.css';
+import Providers from './providers';
 
 export const metadata = {
   title: 'Movies',
   description: 'See your Favorite Movie',
-}
-
+};
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
-      <Providers>
-       {/* Header */}
-       <Header/>
-      
-      {/* Navbar */}
-        <Navbar/>
-        
-      {/* SearchBox */}
+        <Providers>
+          {/* Header */}
+          <Header />
 
-      {children}  
-      </Providers>
+          {/* Navbar */}
+          <Navbar />
+
+          {/* SearchBox */}
+
+          {children}
+        </Providers>
       </body>
     </html>
-  )
+  );
 }
