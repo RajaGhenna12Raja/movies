d332205326df7faada707ec65664420eebb83fed
RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>
2023-03-20 14:55:25 +0530

    added loading effect
    Signed-off-by: RajaGhenna12Raja <104801015+RajaGhenna12Raja@users.noreply.github.com>

---
+++ src/app/loading.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className='flex justify-center'>
+      <Image src='spinner.svg' alt='loading...' height={96} width={96} />
+    </div>
+  );
+}
